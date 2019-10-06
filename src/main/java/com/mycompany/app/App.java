3bc88cca956f6f3b9acbccd28184abf5661de97c
Guruprasadmhl <ec2-user@ip-172-31-43-172.ec2.internal>
2019-10-06 19:04:55 +0000

    added

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+	System.out.println("Hello to the world of Java");
     }
 
     private final String getMessage() {
